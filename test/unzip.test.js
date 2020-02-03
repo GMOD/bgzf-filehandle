092fd2792c58fe3ff9af150ae7df9c53112b2ac4
Colin <colin.diesh@gmail.com>
2020-02-03 15:40:23 -0500

    Add some tests for unzipChunkSlice

--- test/unzip.test.js
+++ test/unzip.test.js
@@ -1,5 +1,10 @@
 const fs = require('fs')
-const { pakoUnzip, nodeUnzip, unzipChunk } = require('../src/unzip')
+const {
+  pakoUnzip,
+  nodeUnzip,
+  unzipChunk,
+  unzipChunkSlice,
+} = require('../src/unzip')
 
 describe('unzip', () => {
   it('can unzip bgzip-1.txt.gz', async () => {
@@ -16,6 +21,36 @@ describe('unzip', () => {
   it('can unzip bgzip-1.txt.gz', async () => {
     const testData = fs.readFileSync(require.resolve('./data/paired.bam'))
     const { dpositions, cpositions } = await unzipChunk(testData)
+    expect(dpositions).toMatchSnapshot()
+    expect(cpositions).toMatchSnapshot()
+  })
+})
+
+describe('unzipChunkSlice', () => {
+  it('can unzip bgzip-1.txt.gz', async () => {
+    const testData = fs.readFileSync(require.resolve('./data/paired.bam'))
+    const { dpositions, cpositions } = await unzipChunkSlice(testData, {
+      minv: { dataPosition: 0, blockPosition: 0 },
+      maxv: { dataPosition: 100, blockPosition: 0 },
+    })
+    expect(dpositions).toMatchSnapshot()
+    expect(cpositions).toMatchSnapshot()
+  })
+  it('can unzip bgzip-1.txt.gz positive minv.dataPosition', async () => {
+    const testData = fs.readFileSync(require.resolve('./data/paired.bam'))
+    const { dpositions, cpositions } = await unzipChunkSlice(testData, {
+      minv: { dataPosition: 50, blockPosition: 0 },
+      maxv: { dataPosition: 100, blockPosition: 0 },
+    })
+    expect(dpositions).toMatchSnapshot()
+    expect(cpositions).toMatchSnapshot()
+  })
+  it('can unzip bgzip-1.txt.gz positive maxv.blockPosition', async () => {
+    const testData = fs.readFileSync(require.resolve('./data/paired.bam'))
+    const { dpositions, cpositions } = await unzipChunkSlice(testData, {
+      minv: { dataPosition: 50, blockPosition: 0 },
+      maxv: { dataPosition: 100, blockPosition: 1 },
+    })
     expect(dpositions).toMatchSnapshot()
     expect(cpositions).toMatchSnapshot()
   })
