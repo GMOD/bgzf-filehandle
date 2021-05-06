144da6b209838e04f337a2b0d4c662ef22678d32
Piotr Gawron <piotr.gawron@uni.lu>
2021-05-06 14:14:15 +0200

    remove fs-extra from code so browserify will work

--- src/localFile.js
+++ src/localFile.js
@@ -1,24 +1,31 @@
+const promisify = require('es6-promisify').promisify
 const fs =
   // eslint-disable-next-line camelcase
-  typeof __webpack_require__ !== 'function' ? require('fs-extra') : undefined
+  typeof __webpack_require__ !== 'function' ? require('fs') : undefined
+
+const fsOpen = fs && fs.open && promisify(fs.open)
+const fsRead = fs && fs.read && promisify(fs.read)
+const fsFStat = fs && fs.fstat && promisify(fs.fstat)
 
 // LocalFile is pretty much just an implementation of the node 10+ fs.promises filehandle,
 // we can switch to that when the API is stable
 class LocalFile {
   constructor(path) {
-    this.fdPromise = fs.open(path, 'r')
+    this.fdPromise = fsOpen(path, 'r')
     this.path = path
   }
 
   async read(buf, offset, length, position) {
     const fd = await this.fdPromise
-    const ret = await fs.read(fd, buf, offset, length, position)
-    return ret
+    const ret = await fsRead(fd, buf, offset, length, position)
+    return {
+      bytesRead: ret,
+    }
   }
 
   async stat() {
     const fd = await this.fdPromise
-    return fs.fstat(fd)
+    return fsFStat(fd)
   }
 }
 
