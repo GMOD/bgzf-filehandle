fbe87474e9297532ed3ebb6202e6ce2621b32a6e
Colin <colin.diesh@gmail.com>
2025-11-24 15:10:10 -0500

    Bench larger file

--- benchmarks/unzip.bench.ts
+++ benchmarks/unzip.bench.ts
@@ -7,14 +7,25 @@ import { unzip as unzipBranch2 } from '../esm_branch2/unzip.js'
 const branch1Name = readFileSync('esm_branch1/branchname.txt', 'utf8').trim()
 const branch2Name = readFileSync('esm_branch2/branchname.txt', 'utf8').trim()
 
-const inputData = new Uint8Array(readFileSync('test/data/T_ko.2bit.gz'))
+const tkoData = new Uint8Array(readFileSync('test/data/T_ko.2bit.gz'))
+const gffData = new Uint8Array(readFileSync('test/data/out.sorted.gff.gz'))
 
 describe('unzip T_ko.2bit.gz (518KB)', () => {
   bench(branch1Name, async () => {
-    await unzipBranch1(inputData)
+    await unzipBranch1(tkoData)
   })
 
   bench(branch2Name, async () => {
-    await unzipBranch2(inputData)
+    await unzipBranch2(tkoData)
+  })
+})
+
+describe('unzip out.sorted.gff.gz (5.2MB)', () => {
+  bench(branch1Name, async () => {
+    await unzipBranch1(gffData)
+  })
+
+  bench(branch2Name, async () => {
+    await unzipBranch2(gffData)
   })
 })
