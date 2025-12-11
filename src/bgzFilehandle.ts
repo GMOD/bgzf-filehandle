fbbb005181db3fbd234e5d61d6f933d0a947d8e2
Colin <colin.diesh@gmail.com>
2025-12-11 13:14:49 -0500

    Avoid stat

--- src/bgzFilehandle.ts
+++ src/bgzFilehandle.ts
@@ -4,6 +4,15 @@ import { concatUint8Array } from './util.ts'
 
 import type { GenericFilehandle } from 'generic-filehandle2'
 
+// BGZF header is 18 bytes: 10 byte gzip header + 6 byte extra field + 2 byte BSIZE
+const BGZF_HEADER_SIZE = 18
+
+function getBgzfBlockSize(header: Uint8Array) {
+  // BSIZE is at bytes 16-17 (little-endian), represents total block size - 1
+  const bsize = header[16]! | (header[17]! << 8)
+  return bsize + 1
+}
+
 export default class BgzFilehandle {
   filehandle: GenericFilehandle
   gzi: GziIndex
@@ -26,20 +35,23 @@ export default class BgzFilehandle {
     compressedPosition: number,
     nextCompressedPosition: number,
   ) {
-    let next = nextCompressedPosition
-    if (!next) {
-      next = (await this.filehandle.stat()).size
+    let blockCompressedLength: number
+    if (nextCompressedPosition) {
+      blockCompressedLength = nextCompressedPosition - compressedPosition
+    } else {
+      // Read the BGZF header to get the block size (avoids needing stat/file size)
+      const header = await this.filehandle.read(
+        BGZF_HEADER_SIZE,
+        compressedPosition,
+      )
+      blockCompressedLength = getBgzfBlockSize(header)
     }
-
-    // read the compressed data into the block buffer
-    const blockCompressedLength = next - compressedPosition
 
     const blockBuffer = await this.filehandle.read(
       blockCompressedLength,
       compressedPosition,
     )
 
-    // uncompress it
     return unzip(blockBuffer)
   }
 
